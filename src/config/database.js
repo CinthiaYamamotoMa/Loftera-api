c1cc17aa213f6f55c77b1fba191b5dbd536d53af
Cinthia <cinthiaym_@outlook.com>
2020-12-08 23:11:29 -0300

    listagem de usuarios semelhantes

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "cabecao01",
+    "password": "",
     "database": "loftera",
     "host": "127.0.0.1",
     "dialect": "mysql",
